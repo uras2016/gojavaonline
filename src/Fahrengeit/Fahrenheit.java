7dea2592463223f33494c35f5e1ad78717cb7410
uras2016 <uras@i.ua>
2016-03-10 22:52:29 +0200

    Fahrengeit to Celsius

---
+++ src/Fahrengeit/Fahrenheit.java
@@ -0,0 +1,20 @@
+package Fahrengeit;
+
+
+import java.util.Scanner;
+
+public class Fahrenheit {
+    public static void main(String[] args){
+        float temp;
+        Scanner in = new Scanner(System.in);// создаем обьект Сканнер, System.in -ввод данных
+
+        System.out.println("Температура по Фаренгейту:");
+        temp = in.nextInt();
+
+        temp = ((temp-32)*5)/9;
+
+        System.out.println("Температура по Цельсию:" + temp);
+
+
+    }
+}
