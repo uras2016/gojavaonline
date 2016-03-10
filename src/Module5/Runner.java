edeecc0ae399cfef055b5aeaeff7bb06b0758bca
uras2016 <uras@i.ua>
2016-03-10 22:46:01 +0200

    println Hello, Java

--- src/Module5/Runner.java
+++ src/Module5/Runner.java
@@ -3,6 +3,6 @@ package Module5;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        System.out.println("Hello, Java!");
     }
 }
