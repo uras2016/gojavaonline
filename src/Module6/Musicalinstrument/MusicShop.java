e07cba11d5a0b755d82897b433b0d9acf9b3c222
uras2016 <uras@i.ua>
2016-03-16 16:41:12 +0200

    Addidtional lection to 4,5,6 modules

--- src/Module6/Musicalinstrument/MusicShop.java
+++ src/Module6/Musicalinstrument/MusicShop.java
@@ -1,31 +1,34 @@
 package module6.Musicalinstrument;
 
 import java.util.List;
+import java.util.Map;
 
 public class MusicShop {
     private List<Trumpet> Trumpet;
     private List<Piano> Piano;
     private List<Guitar> Guitar;
 
-    public List<module6.Musicalinstrument.Trumpet> getTrumpet() {return Trumpet;}
-
-    public void setTrumpet(List<module6.Musicalinstrument.Trumpet> trumpet) {Trumpet = trumpet;
+    public List<module6.Musicalinstrument.Trumpet> getTrumpet() {
+        return Trumpet;
     }
 
     public List<module6.Musicalinstrument.Piano> getPiano() {
         return Piano;
     }
 
-    public void setPiano(List<module6.Musicalinstrument.Piano> piano) {
-        Piano = piano;
-    }
-
     public List<module6.Musicalinstrument.Guitar> getGuitar() {
         return Guitar;
     }
 
-    public void setGuitar(List<module6.Musicalinstrument.Guitar> guitar) {
+    public MusicShop(List<Trumpet> trumpet, List<Piano> piano, List<Guitar> guitar) {
+        Trumpet = trumpet;
+        Piano = piano;
         Guitar = guitar;
     }
+
+
+    public  void prepareInstruments(Map <String, Integer> order){
+    }
+
 
 }
