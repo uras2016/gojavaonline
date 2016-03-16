b1f5574d6b600665b88aa371cdbfa259a1b00c85
uras2016 <uras@i.ua>
2016-03-16 22:37:15 +0200

    add one more time

---
+++ src/Module6/File/ExceptionRun.java
@@ -0,0 +1,32 @@
+package Module6.File;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionRun {
+    public static void main(String[] args) throws PositiveException {
+        System.out.println("Please input quantity of files:");
+        final Scanner scanner = new Scanner(System.in);
+
+        try {
+            int quantity = scanner.nextInt();
+
+
+            if (quantity < 0) {
+                throw new PositiveException(quantity);
+
+            }
+            System.out.println("Quantity:" + quantity);
+        } catch (PositiveException e){
+            System.out.println("Please enter a positive value!");}
+        catch (InputMismatchException ex){
+            System.out.println("Mistake! Only an integer is allowed!!!");
+        }
+
+    }
+
+
+
+
+}
