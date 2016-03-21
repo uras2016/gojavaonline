1bac507fcbb1669834a03a408f2324f6a9f86f86
uras2016 <uras@i.ua>
2016-03-22 00:04:13 +0200

    Starting module 8

---
+++ src/module8/homewrok/Runners.java
@@ -0,0 +1,18 @@
+package module8.homewrok;
+
+
+
+public class Runners {
+    public static void main(String[] args) {
+
+        String reaction;
+        MusicFiles mp3 = new MusicFiles("mp3", 4);
+
+        mp3.open();
+
+        mp3.close("audio single");
+
+
+
+    }
+}
