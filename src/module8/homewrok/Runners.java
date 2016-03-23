5b70a749e09bf6277203b465aecfd9662a45fe84
uras2016 <uras@i.ua>
2016-03-23 23:35:19 +0200

    Homework module 8

--- src/module8/homewrok/Runners.java
+++ src/module8/homewrok/Runners.java
@@ -1,37 +1,63 @@
 package module8.homewrok;
 
 
+
+
+
 import java.util.ArrayList;
-import java.util.Iterator;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Runners {
     public static void main(String[] args) {
 
-        MusicFiles musicFiles = new MusicFiles("mp3", 4);
-        PictureFiles pictureFiles = new PictureFiles("Spring", 15);
-        DocFiles docFiles = new DocFiles("Book", 3);
+
 
         ArrayList bulkRequest = new ArrayList();  // коллекция содержит объекты разных классов
 
-        MusicFiles files;
+
 
         bulkRequest.add(new MusicFiles("mp3", 4));
         bulkRequest.add(new PictureFiles("Spring", 15));
         bulkRequest.add(new DocFiles("Book", 3));
         bulkRequest.add("854");
 
-        System.out.println("111 " + musicFiles.getName());
 
-        System.out.println("111 " + bulkRequest.size());
 
        for (int i = 0; i < bulkRequest.size(); i++){
-           System.out.println("Element " + i + bulkRequest.get(i));
+           System.out.println("Element " + "|" + i + "|" + bulkRequest.get(i) + "|");
        }
 
+        TreeMap <Integer, MusicFiles> albom = new TreeMap<Integer, MusicFiles>();
+
+        albom.put(12, new MusicFiles("Aria", 45));
+        albom.put(98, new MusicFiles("Ballads", 14));
+        albom.put(4, new MusicFiles("Scorpions", 5));
+        albom.put(100, new MusicFiles("Poem", 20));
 
 
+        System.out.println("Keys :" + albom.keySet() + albom.values());
 
-}
+        System.out.println(albom.size());
+
+        for (Map.Entry<Integer, MusicFiles> entry : albom.entrySet()){
+            System.out.println(entry.getKey() + "-" + entry.getValue());
+        }
+        }
 
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
